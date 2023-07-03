004798077d25a171dad844c49002fcab5b036c48
jvschoen <jvshoen@live.com>
2023-07-02 23:37:16 -0700

    removed troublemakers from requirements, slimmed down reqs, started building common utils

---
+++ mle_common/pipeline/ingestor.py
@@ -0,0 +1,19 @@
+# This class should ingest a table object
+#
+
+from abc import abstractmethod
+from mle_common.pipeline.table import Table
+
+class Ingestor:
+    def __init__(self, table: Table):
+        self.table = table
+
+    @abstractmethod
+    def load_df(self):
+        pass
+
+
+class DatabaseIngestor():
+
+    def load_df(self):
+
